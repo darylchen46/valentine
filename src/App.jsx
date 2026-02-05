aa58c01a7cf18253b70a2a5ed3e1b3843b6baf5b
Daryl <darylchen46@gmail.com>
2026-02-05 14:27:49 +0800

    Changed App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import lovesvg from "./assets/All You Need Is Love SVG Cut File.svg";
-import lovesvg2 from "./assets/Love In The Air SVG Cut File.svg";
+import hamsterhold from "./assets/hamsterhold.jpg";
+import hamsterkiss from "./assets/hamsterkiss.gif";
+import gayhamster from "./assets/lovely.gif";
 
 export default function Page() {
   const [noCount, setNoCount] = useState(0);
@@ -19,8 +20,8 @@ export default function Page() {
       "Think again!",
       "Last chance!",
       "Surely not?",
-      "You might regret this!",
-      "Give it another thought!",
+      "Don't be gay!",
+      "PLEASEEEE!",
       "Are you absolutely certain?",
       "This could be a mistake!",
       "Have a heart!",
@@ -41,27 +42,23 @@ export default function Page() {
     <div className="overflow-hidden flex flex-col items-center justify-center pt-4 h-screen -mt-16 selection:bg-rose-600 selection:text-white text-zinc-900">
       {yesPressed ? (
         <>
-          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
+          <img src={hamsterkiss}/>
           <div className="text-4xl md:text-6xl font-bold my-4">
-            Ok Yayyyyy!!!
+            Yayyyyy!!!
           </div>
+          <img 
+            src={gayhamster}
+            className="fixed bottom-10 md:w-40 w-28"
+          />
         </>
       ) : (
         <>
-          <img
-            src={lovesvg}
-            className="fixed animate-pulse top-10 md:left-24 left-6 md:w-40 w-28"
-          />
-          <img
-            src={lovesvg2}
-            className="fixed bottom-16 -z-10 animate-pulse md:right-24 right-10 md:w-40 w-32"
-          />
-          <img
+          <img 
+            src={hamsterhold}
             className="h-[230px] rounded-lg shadow-lg"
-            src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.webp"
           />
           <h1 className="text-4xl md:text-6xl my-4 text-center">
-            Will you be my Valentine?
+            Sarah, Will you be my Valentine?
           </h1>
           <div className="flex flex-wrap justify-center gap-2 items-center">
             <button
@@ -80,22 +77,6 @@ export default function Page() {
           </div>
         </>
       )}
-      <Footer />
     </div>
   );
 }
-
-const Footer = () => {
-  return (
-    <a
-      className="fixed bottom-2 right-2 backdrop-blur-md opacity-80 hover:opacity-95 border p-1 rounded border-rose-300"
-      href="https://github.com/Xeven777/valentine"
-      target="__blank"
-    >
-      Made with{" "}
-      <span role="img" aria-label="heart">
-        ❤️
-      </span>
-    </a>
-  );
-};
